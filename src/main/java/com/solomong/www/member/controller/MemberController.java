6d775fadefda4fd1b584ec2778b8ace16bfb6395
KyleSJ <sj_kyle@naver.com>
2018-05-15 16:02:07 +0900

    no message

--- src/main/java/com/solomong/www/member/controller/MemberController.java
+++ src/main/java/com/solomong/www/member/controller/MemberController.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.solomong.www.member.vo.MemberVO;
+
 @Controller
 public class MemberController {
 	
 	@RequestMapping(value="/loginRegist" , method= RequestMethod.GET)
 	public String viewLoginRegistPage() {
-		return "member/loginRegist";
+		return "member/loginAndRegist";
+	}
+	
+	@RequestMapping(value="loginRegist", method=RequestMethod.POST)
+	public String doLogin(MemberVO member) {
+		
+		System.out.println(member.getMemberId() + " , " + member.getPassword());
+		
+		return "main";
 	}
 
 }
