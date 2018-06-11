ba31de8a7504b7d5c38e2f84c2cd763cf969e075
KyleSJ <sj_kyle@naver.com>
2018-06-11 17:44:48 +0900

    conflict 복구

--- src/main/java/com/solomong/member/service/MemberService.java
+++ src/main/java/com/solomong/member/service/MemberService.java
@@ -1,6 +1,5 @@
 package com.solomong.member.service;
 
-<<<<<<< HEAD
 import com.solomong.member.vo.GoogleMemberVO;
 import com.solomong.member.vo.KakaoMemberVO;
 import com.solomong.member.vo.MemberVO;
@@ -10,8 +9,5 @@ public interface MemberService {
 	public MemberVO readMember(MemberVO memberVO);
 	public boolean registKakaoMember(KakaoMemberVO kakaoMemberVO);
 	public boolean registGoogleMember(GoogleMemberVO googleMemberVO);
-=======
-public interface MemberService {
->>>>>>> master
 
 }
